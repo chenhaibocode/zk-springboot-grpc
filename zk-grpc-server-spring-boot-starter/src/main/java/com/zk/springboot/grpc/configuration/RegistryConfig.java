8067da70a59386d90764897090a10504cd3ed6b8
chenhaibo <chenhaibo001@ke.com>
2018-11-26 15:02:51 +0800

    init project

---
+++ zk-grpc-server-spring-boot-starter/src/main/java/com/zk/springboot/grpc/configuration/RegistryConfig.java
@@ -0,0 +1,42 @@
+package com.zk.springboot.grpc.configuration;
+
+import com.zk.springboot.grpc.registry.ServiceRegistry;
+import com.zk.springboot.grpc.registry.impl.ServiceRegistryImpl;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: chenhaibo
+ * @Date: 2018/11/20 23:10
+ * @Description:
+ */
+@Configuration
+@ConfigurationProperties(prefix = "registry")
+public class RegistryConfig {
+    private String servers;
+    private String nodepath;
+    private String username;
+    private String password;
+
+    @Bean
+    public ServiceRegistry serviceRegistry() {
+        return new ServiceRegistryImpl(servers, nodepath, username, password);
+    }
+
+    public void setServers(String servers) {
+        this.servers = servers;
+    }
+
+    public void setNodepath(String nodepath) {
+        this.nodepath = nodepath;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
